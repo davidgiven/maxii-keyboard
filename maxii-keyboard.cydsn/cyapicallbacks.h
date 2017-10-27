d3e77322a9c0a2c142621417c34180f1e30cab6c
David Given <dg@cowlark.com>
2017-10-27 23:00:58 +0200

    Rework to be simpler, and control the probe lines in software; less lovely soft logic, but easier control. Try to do proper matrix decoding, which isn't working for some reason.

--- maxii-keyboard.cydsn/cyapicallbacks.h
+++ maxii-keyboard.cydsn/cyapicallbacks.h
@@ -1,21 +1,12 @@
-/* ========================================
- *
- * Copyright YOUR COMPANY, THE YEAR
- * All Rights Reserved
- * UNPUBLISHED, LICENSED SOFTWARE.
- *
- * CONFIDENTIAL AND PROPRIETARY INFORMATION
- * WHICH IS THE PROPERTY OF your company.
- *
- * ========================================
-*/
+
+/* maxii-keyboard firmware
+ * (C) 2017 David Given
+ */
+
 #ifndef CYAPICALLBACKS_H
 #define CYAPICALLBACKS_H
     
+#define SenseInterrupt_INTERRUPT_INTERRUPT_CALLBACK 1
+extern void SenseInterrupt_Interrupt_InterruptCallback(void);
 
-    /*Define your macro callbacks here */
-    /*For more information, refer to the Writing Code topic in the PSoC Creator Help.*/
-
-    
-#endif /* CYAPICALLBACKS_H */   
-/* [] */
+#endif
--- maxii-keyboard.cydsn/main.c
+++ maxii-keyboard.cydsn/main.c
@@ -7,20 +7,88 @@
 #include <stdbool.h>
 #include "project.h"
 
-#define PROBES 16
+#define QUEUE_SIZE 32
 
-static uint8 states[PROBES];
+struct queue_entry
+{
+    uint8 x;
+    uint8 y;
+    bool onoff;
+};
 
-static char buffer[16];
+static struct queue_entry queue[QUEUE_SIZE];
+static volatile int readptr = 0;
+static volatile int writeptr = 0;
 
+void SenseInterrupt_Interrupt_InterruptCallback(void)
+{
+    static bool phase = 0;
+    static uint8 probe = 1;
+    static uint8 senses[10] = {};
+    
+    switch (phase)
+    {
+        case false:
+            probe++;
+            if (probe == 11)
+                probe = 1;
+            ProbeReg_Write(probe);
+            break;
+            
+        case true:
+        {
+            uint8 sense = SenseReg_Read();
+            uint8 changed = senses[probe] ^ sense;
+            
+            for (int y=0; y<8; y++)
+            {
+                if (changed & (1<<y))
+                {
+                    if (writeptr != ((readptr-1) & (QUEUE_SIZE-1)))
+                    {
+                        struct queue_entry* entry = &queue[writeptr];
+                        entry->x = probe;
+                        entry->y = y;
+                        entry->onoff = !!(sense & (1<<y));
+                        
+                        writeptr = (writeptr+1) & (QUEUE_SIZE-1);
+                    }
+                    else
+                        LedReg_Write(1);
+                }
+            }
+            
+            senses[probe] = sense;
+            break;
+        }
+    }
+    
+    phase = !phase;
+}
 
 int main(void)
 {
     UART_Start();
+    SenseInterrupt_Start();
     CyGlobalIntEnable;
 
     UART_PutString("GO\r");
+
+    for (;;)
+    {
+        while (readptr == writeptr)
+            ;
+
+        char buffer[32];
+        struct queue_entry* entry = &queue[readptr];
+        sprintf(buffer, "x=%d y=%d o=%d\r", entry->x, entry->y, entry->onoff);
+        UART_PutString(buffer);
+        LedReg_Write(0);
+        
+        readptr = (readptr+1) & (QUEUE_SIZE-1);
+    }
     
+#if 0
     uint8 oldprobe = 0;
     for (;;)
     {
@@ -36,4 +104,5 @@ int main(void)
         sprintf(buffer, "%02x ", sense);
         UART_PutString(buffer);
     }
+#endif
 }
