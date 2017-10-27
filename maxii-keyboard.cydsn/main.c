ea75732de2845287a52707f908212ec68454a3e7
David Given <dg@cowlark.com>
2017-10-28 00:02:39 +0200

    Another attempt to do interrupt-based matrix decoding. There's definitely something odd going on; I get keyboard clashes which I wasn't seeing when doing the brute force scan.

--- maxii-keyboard.cydsn/main.c
+++ maxii-keyboard.cydsn/main.c
@@ -13,61 +13,46 @@ struct queue_entry
 {
     uint8 x;
     uint8 y;
-    bool onoff;
+    uint8 status;
 };
 
 static struct queue_entry queue[QUEUE_SIZE];
 static volatile int readptr = 0;
 static volatile int writeptr = 0;
+static uint8 senses[16] = {};
 
 void SenseInterrupt_Interrupt_InterruptCallback(void)
 {
-    static bool phase = 0;
-    static uint8 probe = 1;
-    static uint8 senses[10] = {};
-    
-    switch (phase)
+    LedReg_Write(0);
+
+    uint8 probe = ProbeReg_Read();
+    uint8 sense = SenseReg_Read();
+    uint8 changed = senses[probe] ^ sense;
+            
+    for (int y=0; y<8; y++)
     {
-        case false:
-            probe++;
-            if (probe == 11)
-                probe = 1;
-            ProbeReg_Write(probe);
-            break;
-            
-        case true:
+        if (changed & (1<<y))
         {
-            uint8 sense = SenseReg_Read();
-            uint8 changed = senses[probe] ^ sense;
-            
-            for (int y=0; y<8; y++)
+            if (writeptr != ((readptr-1) & (QUEUE_SIZE-1)))
             {
-                if (changed & (1<<y))
-                {
-                    if (writeptr != ((readptr-1) & (QUEUE_SIZE-1)))
-                    {
-                        struct queue_entry* entry = &queue[writeptr];
-                        entry->x = probe;
-                        entry->y = y;
-                        entry->onoff = !!(sense & (1<<y));
-                        
-                        writeptr = (writeptr+1) & (QUEUE_SIZE-1);
-                    }
-                    else
-                        LedReg_Write(1);
-                }
+                struct queue_entry* entry = &queue[writeptr];
+                entry->x = probe;
+                entry->y = y;
+                entry->status = !!(sense & (1<<y));
+                
+                writeptr = (writeptr+1) & (QUEUE_SIZE-1);
             }
-            
-            senses[probe] = sense;
-            break;
+            else
+                LedReg_Write(true);
         }
     }
-    
-    phase = !phase;
+                    
+    senses[probe] = sense;
 }
 
 int main(void)
 {
+    LedReg_Write(1);
     UART_Start();
     SenseInterrupt_Start();
     CyGlobalIntEnable;
@@ -81,28 +66,10 @@ int main(void)
 
         char buffer[32];
         struct queue_entry* entry = &queue[readptr];
-        sprintf(buffer, "x=%d y=%d o=%d\r", entry->x, entry->y, entry->onoff);
+        sprintf(buffer, "x=%d y=%d o=%d\r", entry->x, entry->y, entry->status);
         UART_PutString(buffer);
         LedReg_Write(0);
         
         readptr = (readptr+1) & (QUEUE_SIZE-1);
     }
-    
-#if 0
-    uint8 oldprobe = 0;
-    for (;;)
-    {
-        uint8 probe = ProbeReg_Read();
-        if (probe == oldprobe)
-            continue;
-        oldprobe = probe;
-
-        if (probe == 0)
-            UART_PutString("\r");
-            
-        uint8 sense = SenseReg_Read();
-        sprintf(buffer, "%02x ", sense);
-        UART_PutString(buffer);
-    }
-#endif
 }
